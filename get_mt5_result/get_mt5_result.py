fcbdabef825c10a2faa527c1bc53b2d314351315
mattchunghk <mattchunghk@gmail.com>
2024-03-02 19:54:53 +0800

    get result

--- get_mt5_result/get_mt5_result.py
+++ get_mt5_result/get_mt5_result.py
@@ -4,7 +4,7 @@ import pytz
 import os
 from dotenv import load_dotenv
 # Get the path to the directory this file is in
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.dirname(__file__)
 
 # Connect the path with your '.env' file name
 load_dotenv(os.path.join(BASEDIR, '.env'))
